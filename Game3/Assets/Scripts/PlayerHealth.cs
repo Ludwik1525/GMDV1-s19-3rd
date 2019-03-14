adf82aa8e7e508fa1f9473f10a7de3ccb747c93f
RuneSChr <runechr2014@gmail.com>
2019-03-14 17:09:34 +0100

    Added a canvas to the main camera and healthscript
    canvas is going to contain health bars for the different teams, script is going to manage

---
+++ Game3/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	private Canvas _canvas;
+	private Text _hp;
+	private int _hpNumber;
+	public GameObject _user, _canvasObject;
+	private RectTransform rectTranny;
+
+
+	//Using awake to set the health number on the player prefabs
+	void Awake()
+	{
+		print("Awake");
+		_hpNumber = 100;
+	}
+	// Use this for initialization
+	void Start () {
+		print("Start");
+		_user = GetComponent<GameObject>();
+		print(_user);
+		AddCanvasAndText();
+	}
+
+	void AddCanvasAndText()
+	{
+		print("addCanvasReached");
+		if(_user){
+			print("Hey");
+			print(_user.GetComponent<GameObject>());
+		}
+	}
+	
+	
+}
