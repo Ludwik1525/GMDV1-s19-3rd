718ad9fa89b85a833329173c2cf518e116d5f2d7
Ludwik1526 <mmichal1526@gmail.com>
2019-03-17 20:03:55 +0100

    Player controls menu

--- Game3/Assets/Scripts/MainMenuButtons.cs
+++ Game3/Assets/Scripts/MainMenuButtons.cs
@@ -8,25 +8,31 @@ public class MainMenuButtons : MonoBehaviour
 {
 
     public Button startB;
+    public Button controls;
     public Button optionsB;
     public Button exitB;
     public Button back;
+    public Button backA;
     public Slider slider;
 
     public GameObject options;
     public GameObject mainStuff;
+    public GameObject controlsStuff;
 
     void Start () {
         options.gameObject.SetActive(false);
+        controlsStuff.gameObject.SetActive(false);
         slider.value = 0.5f;
     }
 	
 	void Update () {
 		
         startB.onClick.AddListener(StartGame);
+        controls.onClick.AddListener(OpenControls);
         optionsB.onClick.AddListener(OpenOptions);
         exitB.onClick.AddListener(QuitGame);
         back.onClick.AddListener(GoBack);
+        backA.onClick.AddListener(GoBackA);
         AudioListener.volume = slider.value;
 
     }
@@ -47,10 +53,22 @@ public class MainMenuButtons : MonoBehaviour
         options.gameObject.SetActive(true);
     }
 
+    void OpenControls()
+    {
+        mainStuff.gameObject.SetActive(false);
+        controlsStuff.gameObject.SetActive(true);
+    }
+
     void GoBack()
     {
         options.gameObject.SetActive(false);
         mainStuff.gameObject.SetActive(true);
     }
-    
+
+    void GoBackA()
+    {
+        controlsStuff.gameObject.SetActive(false);
+        mainStuff.gameObject.SetActive(true);
+    }
+
 }
