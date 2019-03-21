f28522f7a2d0ad0de3c5bca726a087a4356db25a
RuneSChr <runechr2014@gmail.com>
2019-03-21 15:15:50 +0100

    updated teamhealth
    now supports team recognition

--- Game3/Assets/Scripts/TeamHealth.cs
+++ Game3/Assets/Scripts/TeamHealth.cs
@@ -31,7 +31,7 @@ public class TeamHealth : MonoBehaviour {
 
     void Update()
     {
-        teamColor = turnMaker.GetColor();
+        // teamColor = turnMaker.GetColor();
     }
 	//Sets the size of the bar gameobject which is representing the healthbar of the teams
 	//method should perhaps be called setHealth
@@ -52,6 +52,7 @@ public class TeamHealth : MonoBehaviour {
 		print(_totalHp + "total");
 		print(_totalHpFirst + "first");
 		relation = _totalHp/_totalHpFirst;
+		print(relation);
 		bar.localScale = new Vector3((relation), 1f);			
 		healthbarText.text = (relation * 100) + "%";
 		
@@ -60,21 +61,41 @@ public class TeamHealth : MonoBehaviour {
 	public void setTotalHp(){
        //Check here if there any active players and go to win scene
 				// print(transform.name);
-			foreach(Transform playertrans in transform){
 				
-				//maybe make check to see if found player is "active"
-				if(playertrans.CompareTag("Player")){
-					print(playertrans);
-				playerHealth = playertrans.GetComponent<PlayerHealth>();				
-					
-				_totalHp += playerHealth.getPlayerHp();	
-					
-				}
+				string teamBlue = "Blue";
+				string teamGreen = "Green";
+				string team = "Team";
+				string teamColor = transform.name.Substring(9);
+				
+			//solution is to move the script to the camera 
 
-			}
+				
+			// foreach(Transform playertrans in transform){
+				
+			// 	//maybe make check to see if found player is "active"
+			// 	if(playertrans.CompareTag("Player")){
+			// 		print(playertrans.name + "player tranny");
+			// 	playerHealth = playertrans.GetComponent<PlayerHealth>();				
+					
+			// 	_totalHp += playerHealth.getPlayerHp();	
+					
+			// 	}
+
+			// }
 			print(_totalHp);
 			print(_totalHpFirst);
 			_totalHpFirst = _totalHp;		
+
+	}
+	//you just need to use this method to determine the current team name
+	//then you have to use this method in "SettotalHp" so that the method can see what team healthbar to manipulate
+	public string lookForTeam(string teamColor){
+		if(teamColor == "Blue"){
+			return "TeamBlue";
+		}else {
+			return "TeamGreen";
+		}
+
 
 	}
 	
